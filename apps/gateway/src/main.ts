0c2f8b9235a35e5f33e60fbcb8045706e7fc54d6
谢彪 <xiebiaoa@dcits.com>
2021-10-31 10:27:36 +0800

    增加验证管道

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 使用全局验证管道
+  app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('DcAdmin')
--- libs/common/src/dtos/core/create-user-by-local.dto.ts
+++ libs/common/src/dtos/core/create-user-by-local.dto.ts
@@ -1,9 +1,16 @@
+import { IsEnum, Length } from 'class-validator';
 import { GenderEnum } from '@app/common/enums/gender.enum';
 
 export class CreateUserByLocalDto {
+  @Length(1, 25)
   account: string;
+
   password: string;
+
   userName?: string;
+
+  @IsEnum(GenderEnum)
   gender?: GenderEnum;
+
   birthday?: Date;
 }
